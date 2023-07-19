2d334ee796aaa1e554e19dbd4fbc6d180bea85ed
CcoutinhoM <systemsmanagerlda@gmail.com>
2023-07-19 05:02:51 +0100

    Nova alteração 11

--- app/detalhesServico/[id]/page.tsx
+++ app/detalhesServico/[id]/page.tsx
@@ -71,7 +71,7 @@ function DetalhesServicos({ params }: any) {
               <>
                 <CardContent>
                   
-                      <div key={autor.creator._id} className='flex flex-row content-center justify-center rounded-full text-center'>
+                      <div key={'548'} className='flex flex-row content-center justify-center rounded-full text-center'>
                         <Image className='rounded-full' width={100} height={100} src={autor.creator.image} alt="Perfil Image" />              
                         </div>
                         <br />
